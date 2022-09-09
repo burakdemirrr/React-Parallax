ab5a3ac3fa52143d2c2d0c392359303d04733960
Burak <mburak.demir.059@gmail.com>
2022-09-09 14:38:07 +0300

    image

--- src/App.js
+++ src/App.js
@@ -5,21 +5,26 @@ import { Parallax,ParallaxLayer } from '@react-spring/parallax';
 import cheeseburger from "./320px-Cheeseburger.png"
 import wine from "./png-clipart-clear-wine-glass-red-wine-wine-glass-cup-red-wine-glass-wine-removebg-preview.png"
 import pizza from "./png-transparent-pizza-slice-pizza-italian-cuisine-sicilian-pizza-pizza-margherita-garlic-bread-takeout-pizza-by-the-slice-removebg-preview.png"
+import { useRef } from 'react';
 function App() {
+  const ref=useRef();
   return (
     <div className="App">
-      <Header/>
+      
       <div className='parallax'>
-      <Parallax pages={3}>
-        <ParallaxLayer className='first' sticky={{start:0,end:1.2}}>
-          <p>BEST THING</p>
-          <p>YOU  TASTE</p>
+      <Parallax pages={3}  ref={ref}>
+        <ParallaxLayer className='first' sticky={{start:0,end:0.3}} speed={1}>
+          <p>BEST THING <br /> YOU TASTE</p>
         </ParallaxLayer>
 
-        <ParallaxLayer offset={0.8} speed={1}>
+        <ParallaxLayer offset={0.8} speed={1} className="images">
           <img src={cheeseburger} alt="" />
           <img src={pizza} alt="" />
           <img src={wine} alt="" />
+        </ParallaxLayer>
+
+        <ParallaxLayer>
+
         </ParallaxLayer>
 
       </Parallax>
