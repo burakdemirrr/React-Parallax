9a955a89c0523a07c6fb3bccb4e65f01095ae6f9
Burak <mburak.demir.059@gmail.com>
2022-09-09 14:46:48 +0300

    Video implement

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import cheeseburger from "./320px-Cheeseburger.png"
 import wine from "./png-clipart-clear-wine-glass-red-wine-wine-glass-cup-red-wine-glass-wine-removebg-preview.png"
 import pizza from "./png-transparent-pizza-slice-pizza-italian-cuisine-sicilian-pizza-pizza-margherita-garlic-bread-takeout-pizza-by-the-slice-removebg-preview.png"
 import { useRef } from 'react';
+import video from "./mixkit-hamburger-with-french-fries-and-soda-29710-large.mp4"
 function App() {
   const ref=useRef();
   return (
@@ -14,7 +15,7 @@ function App() {
       <div className='parallax'>
       <Parallax pages={3}  ref={ref}>
         <ParallaxLayer className='first' sticky={{start:0,end:0.3}} speed={1}>
-          <p>BEST THING <br /> YOU TASTE</p>
+          <p>BEST THING <span > YOU TASTE</span></p>
         </ParallaxLayer>
 
         <ParallaxLayer offset={0.8} speed={1} className="images">
@@ -23,8 +24,10 @@ function App() {
           <img src={wine} alt="" />
         </ParallaxLayer>
 
-        <ParallaxLayer>
-
+        <ParallaxLayer offset={1.3} speed={0.3} factor={2}>
+          <video  autoPlay muted loop>
+            <source src={video} type="video/mp4" />
+          </video>
         </ParallaxLayer>
 
       </Parallax>
